b49cd538d5af8ccec664a2c246520978a52e09a3
andreyscott <andreyscott301@gmail.com>
2024-02-13 20:50:13 +0100

    added some quick fixes

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -25,7 +25,7 @@ class MyButton extends StatelessWidget {
                 style: const TextStyle(color: Colors.white),
               ),
               const SizedBox(
-                width: 10,
+                width: 15,
               ),
               const Icon(
                 Icons.arrow_forward,
--- lib/models/shop.dart
+++ lib/models/shop.dart
@@ -16,14 +16,21 @@ class Shop extends ChangeNotifier {
         price: 35,
         rating: 4.5,
         description:
-            "A platter of sushi with a variety of fish and vegetables.  Celebrate the pure artistry of fresh seafood with our sashimi platters, showcasing the natural beauty and delicate flavors of each cut."),
+            "A platter of sushi with a variety of fish and vegetables.  Celebrate the pure artistry of fresh seafood with our sashimi platters, showcasing the natural beauty and delicate flavors of each cut of fish. Our chefs select the finest fish from around the world, expertly slicing and presenting each piece to highlight its unique qualities."),
     Food(
         name: "Ramen noodles",
-        imagePath: "lib/images/sushi3.png",
+        imagePath: "lib/images/ramen.png",
         price: 39,
         rating: 4.8,
         description:
             "A bowl of tasty ramen noodles with pork, egg, and vegetables. Slurp your way through our comforting ramen broths, simmered for hours and bursting with umami. Choose from classic shoyu to adventurous tantan, each customizable with an array of toppings."),
+    Food(
+        name: "Poke Bowl",
+        imagePath: "lib/images/sushi3.png",
+        price: 29,
+        rating: 4.7,
+        description:
+            "A bowl of poke with fresh fish, rice, and vegetables.  Dive into our vibrant poke bowls, brimming with fresh fish, colorful vegetables, and bold sauces. Each bowl is a unique culinary adventure and a feast for the eyes."),
   ];
 
   final List<Food> _cart = [];
